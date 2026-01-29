9871077e0c5f2f09b6b83d632d8146c7fb21b906
Andi Bahdar <andibahdar@Mac-Deceng.local>
2026-01-29 17:00:31 +0700

    belajar hari pertama

---
+++ main.py
@@ -0,0 +1,174 @@
+a=15
+b=10
+pendek=15
+
+print("nilai a adalah: ",a)
+print("nilai b adalah: ",b)
+print("nilai pendek adalah: ",pendek)
+
+nilai_z=1000
+niali_x=1629
+jumlah91=100000002020
+
+print("nilai a = ",a)
+a=801
+print("nilai a = ",a)
+
+print("nilai a = ",a)
+a=801
+print("nilai a = ",a)
+print("nilai b = ",b)
+b=801
+print("nilai b = ",b)
+
+#ini adalah data integer
+data_string=111625
+print("data : ",data_string)
+print("-bertipe : ",type(data_string))
+#ini adalah data float
+data_float=13.298
+print("data : ",data_float)
+print("-bertipe : ",type(data_float))
+#ini adalah data string
+data_string="andi bachdar dd"
+print("data : ",data_string)
+print("-bertipe : ",type(data_string))
+#ini adalah data bolean
+data_boolean=True
+print("data : ",data_boolean)
+print("-bertipe : ",type(data_boolean))
+
+#tipe data khusus
+#tipe complex
+data_complex=complex(5,7)
+print("data : ",data_complex)
+print("-bertipe : ",type(data_complex))
+from ctypes import c_double
+#tipe complex
+data_c_double=c_double(50.1777)
+print("data : ",data_c_double)
+print("-bertipe : ",type(data_c_double))
+#materi casting
+#casting adsalah mengubah suatu variable ke tipe data lain
+print("====integer====")
+data_int=9881;
+print("data = ",data_int,",type= ",type(data_int))
+#mengubah tipe data/conversi tipe data
+data_string=str(data_int)
+data_float=float(data_int)
+data_bool=bool(data_int)
+print("data = ",data_string,",type= ",type(data_string))
+print("data = ",data_float,",type= ",type(data_float))
+print("data = ",data_bool,",type= ",type(data_bool))
+
+print("====float====")
+data_float=9881;
+print("data = ",data_float,",type= ",type(data_float))
+#mengubah tipe data/conversi tipe data
+data_string=str(data_float)
+data_int=int(data_float)
+data_bool=bool(data_float)
+print("data = ",data_string,",type= ",type(data_string))
+print("data = ",data_int,",type= ",type(data_int))
+print("data = ",data_bool,",type= ",type(data_bool))
+
+print("====string====")
+data_str=9881;
+print("data = ",data_str,",type= ",type(data_str))
+#mengubah tipe data/conversi tipe data
+data_float=float(data_str)
+data_int=int(data_str)
+data_bool=bool(data_str)
+print("data = ",data_float,",type= ",type(data_float))
+print("data = ",data_int,",type= ",type(data_int))
+print("data = ",data_bool,",type= ",type(data_bool))
+print("====boolean====")
+
+data_bool=9881;
+print("data = ",data_bool,",type= ",type(data_bool))
+#mengubah tipe data/conversi tipe data
+data_float=float(data_bool)
+data_int=int(data_bool)
+data_str=str(data_str)
+print("data = ",data_float,",type= ",type(data_float))
+print("data = ",data_int,",type= ",type(data_int))
+print("data = ",data_str,",type= ",type(data_str))
+#memasukkan input kepada variable dan menkonvertnya ke tipe data yang kita inginkan
+#data yang dimasukkan string
+data=input("masukkan nama anda : ")
+print("data adalah : ",data,"type = ",type(data))
+#data yang dimasukkan int atau float
+angka_int=int(input("masukkan data integer : "))
+print("angka yang kamu masukkan adalah: ",angka_int,"type = ",type(angka_int))
+#data yang anda masukkan float
+data_float=float(input("masukkan data float: "))
+print("data float yang anda masukkan: ",data_float,"type: ",data_float)
+#data yang anda masukkan bollean
+data_bool=bool(int(input("masukkan data boolean: ")))
+print("data bollean yang anda masukkan: ",data_bool,"type: ",data_bool)
+
+#operasional aritmatika
+a=15
+b=13
+#operasi penjumlahan +
+hasil=a+b
+print(a,"+",b,"=",hasil)
+#operasi pengurangan -
+hasil=a-b
+print(a,"-",b,"=",hasil)
+#operasi perkalian *
+hasil=a*b
+print(a,"*",b,"=",hasil)
+#operasi pembagian /
+hasil=a/b
+print(a,"/",b,"=",hasil)
+#operasi eksponen **
+hasil=a**b
+print(a,"**",b,"=",hasil)
+#operasi modulus %
+hasil=a%b
+print(a,"%",b,"=",hasil)
+#operasi floor devesion //
+hasil=a//b
+print(a,"//",b,"=",hasil)
+
+#operasi prioritas
+"""
+    1.()
+    2.eksponen **
+    3.perkalian dan teman temanya * / % // **
+    4.pertambahan dan pengurangan + -
+"""
+x=5
+y=4
+z=3
+hasil= x** y * z + x / y - y % z // x
+print(x,"**",y,"*",z,"+",x,"/",y,"-",y,"%",z,"//",x,"=",hasil)
+#urutan akan di prioritaskan ke penjumlahan yang di dalam kurung terlebih dahulu
+hasil=x+y*z
+print(x,"+",y,"*",z,"=",hasil)
+#kurung akan mengambil langkah yang paling awal
+hasil = (x + y) * z
+print('(',x,'+',y,') * ',z,'=',hasil)
+
+#program konversi temperatur
+print("\nPROGRAM KONVERSI TEMPRATUR\n")
+#celcius
+celcius=float(input('Masukkan suhu dalam Celcius: '))
+print('suhu yang anda masukkan adalah',celcius,'Celcius')
+#fahrenheit
+fahrenheit=((9/5)*celcius)+32
+print('suhu dalam fahrenheit adalah',fahrenheit,'fahrenheit')
+#reamur
+reamur=(4/5)*celcius
+print('suhu dalam reamur adalah',reamur,'reamur')
+#kelvin
+kelvin=celcius+273
+print('suhu dalam kelvin adalah',kelvin,'kelvin')
+
+#pr konversi kelvin ke fahrenheit
+fahrenheitkonversi= (kelvin - 273.15) * 9/5 + 32
+print('suhu dalam fahrenheit konversi ke kelvin adalah',fahrenheitkonversi,'fahrenheit')
+#pr konversi fahrenheit ke kelvin
+kelvinkonversi=(fahrenheit -32)*5/9+273.15
+print('suhu dalam kelvin konversi ke fahrenheit adalah',kelvinkonversi,'kelvin')
