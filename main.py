fdfc9f807a98e7fbd83a50c2cae49ccf50a520ee
Andi Bahdar <andibahdar@Mac-Deceng.local>
2026-01-30 16:19:48 +0700

    belajar hari ke3

--- main.py
+++ main.py
@@ -257,3 +257,67 @@ print("nilai x = ",x,"id",hex(id(x)))
 print("nilai y = ",y,"id",hex(id(y)))
 hasil= x is not y
 print('x is y =',hasil)
+
+#operasi logika atau boleean
+# not, or, and, xor
+#not
+print("=====NOT=====")
+a=True
+c= not a
+print("data a = ",a)
+print("-----NOT")
+print("data c = ",c)
+#OR(jika salah satu true maka outpunya true )
+print("====OR====")
+a=True
+b=False
+c= a or b
+print(a,"OR",b," =",c)
+a=True
+b=True
+c= a or b
+print(a,"OR",b,"  =",c)
+a=False
+b=False
+c= a or b
+print(a,"OR",b,"=",c)
+a=False
+b=True
+c= a or b
+print(a,"OR",b," =",c)
+# AND(jika salah satu False maka outpunya False)
+print("====AND====")
+a=True
+b=False
+c= a and b
+print(a,"AND",b," =",c)
+a=True
+b=True
+c= a and b
+print(a,"AND",b,"  =",c)
+a=False
+b=False
+c= a and b
+print(a,"AND",b,"=",c)
+a=False
+b=True
+c= a and b
+print(a,"AND",b," =",c)
+#XOR(akan True jika salah satu true,sisanya False )
+print("====XOR====")
+a=True
+b=False
+c= a ^ b
+print(a,"XOR",b," =",c)
+a=True
+b=True
+c= a ^ b
+print(a,"XOR",b,"  =",c)
+a=False
+b=False
+c= a ^ b
+print(a,"XOR",b,"=",c)
+a=False
+b=True
+c= a ^ b
+print(a,"XOR",b," =",c)
